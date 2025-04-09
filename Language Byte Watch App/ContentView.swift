8430a10c63a697c0fb4e5507eb7773aef6b9f11b
Kai Wen Franklin <kaiwen.f@gmail.com>
2025-04-08 22:51:14 -0400

    Initial Commit

---
+++ Language Byte Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Language Byte Watch App
+//
+//  Created by Kevin Franklin on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Language Byte Watch App/Language_ByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Language_ByteApp.swift
+//  Language Byte Watch App
+//
+//  Created by Kevin Franklin on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Language_Byte_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
